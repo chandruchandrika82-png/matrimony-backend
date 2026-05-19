b1ae3e042418a06bedf2c44a1a9e59ebcc38e25c
Chandrika Chandru <chandruchandrika82@gmail.com>
2026-05-19 15:20:57 +0530

    Fix Render port

--- server.js
+++ server.js
@@ -200,6 +200,8 @@ app.get("/api/messages/:user1/:user2", async (req, res) => {
 // ======================
 // 🚀 SERVER
 // ======================
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
